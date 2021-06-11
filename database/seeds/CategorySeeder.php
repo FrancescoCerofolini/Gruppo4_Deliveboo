cd2ac6c4e966de46e921af317b069183aed31f82
laura-caldaronello <caldaronello.l@gmail.com>
2021-06-11 11:22:24 +0200

    Category Seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +12,13 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = ['Pizza','Cinese','Giapponese','Pesce','Messicano','Barbecue','Sushi','Gelato','Piadina','Hamburger','Panini','Colazione','Vegetariano','Vegano','Dessert','Greco','Americano','Italiano','Carne','Kebab','Osteria'];
+        sort($categories);
+        
+        for ($i = 0; $i < count($categories); $i++) {
+            $new_category = new Category();
+            $new_category->name = $categories[$i];
+            $new_category->save();
+        }
     }
 }
