b73fe39f9c1f65ca4eb1f4a919548d1c398abde4
FrancescoCerofolini <f.cerofolini96@gmail.com>
2021-06-11 16:17:31 +0200

    Category-Dish-User-seeder

--- database/seeds/DishSeeder.php
+++ database/seeds/DishSeeder.php
@@ -18,8 +18,8 @@ class DishSeeder extends Seeder
         for ($i = 0; $i < count($data[0]); $i++) {
             $newdish = new Dish;
             $newdish->name = $data[0][$i];
-            $newdish->user_id = 1; //floor($i/5) + 1;
-            $newdish->description = "allergeni: "; //. $data[1][rand(0, count($data[1]) -1)];
+            $newdish->user_id = floor($i/5) + 1;
+            $newdish->description = "allergeni: " . $data[1][rand(0, count($data[1]) -1)];
             $newdish->price = $faker->randomFloat(2, 1, 99);
             $newdish->visibility = true;
             $newdish->save();
