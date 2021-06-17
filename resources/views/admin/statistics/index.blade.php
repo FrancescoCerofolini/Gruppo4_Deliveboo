9846a6b4caf3f16506505ab81352862598c1224f
laura-caldaronello <caldaronello.l@gmail.com>
2021-06-17 15:34:28 +0200

    grafici praticamente ok

--- resources/views/admin/statistics/index.blade.php
+++ resources/views/admin/statistics/index.blade.php
@@ -4,21 +4,58 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                    <canvas id="myChart" height="500" width="700"></canvas>
+                    <canvas id="myChart1" height="500" width="700"></canvas>
+                    <canvas id="myChart2" height="500" width="700"></canvas>
             </div>
         </div>
     </div>
 
 
     <script>
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
+        var ctx1 = document.getElementById('myChart1').getContext('2d');
+        var myChart1 = new Chart(ctx1, {
             type: 'bar',
             data: {
                 labels: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
                 datasets: [{
                     label: 'Ordini ultimo anno',
-                    data: <?php echo json_encode($orders_pretty); ?>,
+                    data: <?php echo json_encode($orders_by_month_pretty); ?>,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        var ctx2 = document.getElementById('myChart2').getContext('2d');
+        var myChart2 = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: ['2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'],
+                datasets: [{
+                    label: 'Ordini ultimi 10 anni',
+                    data: <?php echo json_encode($orders_by_year_pretty); ?>,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
