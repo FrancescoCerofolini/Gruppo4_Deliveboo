fef066932e455db22a6ea6cd8d6e1b49a36e48f6
laura-caldaronello <caldaronello.l@gmail.com>
2021-06-17 14:20:06 +0200

    OK QUERY

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -11,9 +11,26 @@ use App\Order;
 class StatisticsController extends Controller
 {
     public function index() {
-        $orders = DB::table('orders')->whereYear('created_at', '=',2021)->get();
+        /* $orders_by_year = [];
+        $counter_array = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+        for ($counter = 0; $counter < count($counter_array); $counter++) {
+            $orders_by_year[$counter] = DB::table('orders')->select('created_at')->whereYear('created_at','=',2021)->whereMonth('created_at','=',$counter_array[$counter])->get();
+        } */
+
+        $orders = DB::table('orders')
+            ->selectRaw('COUNT(DISTINCT(orders.id)) AS orders,MONTH(orders.created_at) AS month')
+            ->join('order_dish','order_dish.order_id','=','orders.id')
+            ->join('dishes','dishes.id','=','order_dish.dish_id')
+            ->where('dishes.user_id',1)
+            ->whereYear('orders.created_at','=',2021)
+            ->groupBy('month')
+            ->get();
+            
+            /* ->count('orders.id'); */
+            
+            /* ->whereYear('created_at','=',2021)->groupBy(DB::raw('MONTH(created_at)'))->get(); */
         
-        //@dd($orders);
+        @dd($orders);
         return view('admin.statistics.index');
     }
 }
