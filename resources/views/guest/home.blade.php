30ebc85956f8dae98a5a02738ee6879dbf5cf8cb
mikpi86 <piras.michela86@gmail.com>
2021-06-21 16:49:29 +0200

    navbar

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -7,11 +7,15 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"             rel="stylesheet">
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"     integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="     crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Styles -->
         <style>
-            html, body {
+ /*            html, body {
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
@@ -64,64 +68,87 @@
 
             .my-hidden {
                 display: none;
-            }
+            } */
         </style>
+
+        <!-- Style SASS -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div id='app' class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/admin') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div id='app' class="">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <head>
+                {{-- navbar --}}
+                <div class="my-navbar">
+                    <div class="row">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    DeliveBoo
-                </div>
-                <h2 class='category'>@{{ selected_category }}</h2>
-                <div class="ristoranti">
-                    <span v-if='flag != false' v-for='(restaurant, index) in restaurants'>
-                        <form action="{{ route('order.create')}}" method="get">
-                            <div class=" form-group my-hidden">
-                                <input name="user_id" type="text" :value="(restaurant.user_id != '') ? restaurant.user_id : 'default'">
-                                <input name="user_slug" type="text" :value="(restaurant.slug != '') ? restaurant.slug : 'default'">
-                            </div>
-                            <button type="submit">ordina da @{{restaurant.slug}}</button>
+                        <div class="col-xs-4 col-md-4 logo debug">
+                            <a href="{{ route('guest-home') }}">
+                                <img src="{{ asset('img/DeliveBoo_logo.png') }}" alt="DeliveBoo logo">
+                            </a>
+                        </div>
     
-                        </form>
-                    </span>
-                    <span v-for='(element, index) in collection' v-if='flag == false'>
-                        <form action="{{ route('order.create')}}" method="get">
-                            <div class=" form-group my-hidden">
-                                <input name="user_id" type="text" :value="(element.user_id != '') ? element.user_id : 'default'">
-                                <input name="user_slug" type="text" :value="(element.slug != '') ? element.slug : 'default'">
-                            </div>
-                            <button type="submit">ordina da @{{element.slug}}</button>
+                        <div class="col-xs-3 col-md-4 text-center debug">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <span>Milano</span>
+                        </div>
     
+                        @if (Route::has('login'))
+                            <div class="col-xs-5 col-md-4 text-right debug">
+                                @auth
+                                    <a href="{{ url('/admin') }}">Home</a>
+                                @else
+                                    <a href="{{ route('login') }}">Login</a>
+            
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}">Registrati</a>
+                                    @endif
+                                @endauth
+                            </div>
+                        @endif                
+                    </div>
+                </div>
+            </head>
+
+            <main>
+                <div class="content container-fluid text-center">
+                    <h2 class='category'>@{{ selected_category }}</h2>
+                    <div class="ristoranti">
+                        <span v-if='flag != false' v-for='(restaurant, index) in restaurants'>
+                            <form action="{{ route('order.create')}}" method="get">
+                                <div class=" form-group my-hidden">
+                                    <input name="user_id" type="text" :value="(restaurant.user_id != '') ? restaurant.user_id : 'default'">
+                                    <input name="user_slug" type="text" :value="(restaurant.slug != '') ? restaurant.slug : 'default'">
+                                </div>
+                                <button type="submit">ordina da @{{restaurant.slug}}</button>
+        
+                            </form>
+                        </span>
+                        <span v-for='(element, index) in collection' v-if='flag == false'>
+                            <form action="{{ route('order.create')}}" method="get">
+                                <div class=" form-group my-hidden">
+                                    <input name="user_id" type="text" :value="(element.user_id != '') ? element.user_id : 'default'">
+                                    <input name="user_slug" type="text" :value="(element.slug != '') ? element.slug : 'default'">
+                                </div>
+                                <button type="submit">ordina da @{{element.slug}}</button>
+        
+                            </form>
+                        </span>
+                    </div>
+                    <div v-if='selected_category != ""' class="searchbar">
+                        <form action="" method=''>
+                            <input v-model='searchFilter'  type="text">
+                            <input v-on:click='searchResults' type="button" value='Search'>
+                            <input v-on:click='resetRestaurants' type="button" value='reset'>
                         </form>
-                    </span>
+                    </div>
+                    <div v-if='selected_category == ""' class="categories">
+                        <button type='sumbit'  v-on:click='getRestaurants(category)' v-for='(category,index) in categories'>@{{category.name}}</button type='sumbit'>
+                    </div>
                 </div>
-                <div v-if='selected_category != ""' class="searchbar">
-                    <form action="" method=''>
-                        <input v-model='searchFilter'  type="text">
-                        <input v-on:click='searchResults' type="button" value='Search'>
-                        <input v-on:click='resetRestaurants' type="button" value='reset'>
-                    </form>
-                </div>
-                <div v-if='selected_category == ""' class="categories">
-                    <button type='sumbit'  v-on:click='getRestaurants(category)' v-for='(category,index) in categories'>@{{category.name}}</button type='sumbit'>
-                </div>
-            </div>
+            </main>
         </div>
     </body>
 </html>
