dcb0d7e87c8e11d892b36ca924f5980742ac1459
laura-caldaronello <caldaronello.l@gmail.com>
2021-06-24 11:59:11 +0200

    DA MERGIARE

--- resources/views/guest/order/create.blade.php
+++ resources/views/guest/order/create.blade.php
@@ -14,7 +14,7 @@
                     <input type="text" name="user_id" class="form-control" value="{{$user_id}}">
                 </div> --}}
                 {{-- CARRELLO --}}
-            <form id="myform" action="{{route('guest.payment')}}"> {{-- QUESTA È STATA CAMBIATA DA LAURA MENTRE PRIMA ESEGUIVA IL BOTTONE PAY CON --}}
+            <form id="myform" action="{{route('guest.payment')}}"> {{-- QUESTA È STATA CAMBIATA DA LAURA MENTRE PRIMA ESEGUIVA IL BOTTONE PAY CON UNO SCRIPT JAVASCRIPT --}}
                  <div v-show="flag_cart" class="form-group">
                      {{-- @foreach ($dishes as $dish)  --}}
                         <div v-for="(quantity, index) in quantity_dish" v-if="quantity_dish[index] > 0">
