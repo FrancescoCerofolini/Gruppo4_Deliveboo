a51061c843893156cf29a6c88f5deced1ac9645b
Emilio994 <emilio.994@gmail.com>
2021-06-24 15:46:45 +0200

    piccole modifiche in responsive

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,19 +3,19 @@
     <div class="my-navbar">
         <div class="row">
 
-            <div class="col-xs-4 col-md-4 logo">
+            <div class="col-4 col-md-4 logo">
                 <a href="{{ route('guest-home') }}">
                     <img src="{{ asset('img/DeliveBoo_logo.png') }}" alt="DeliveBoo logo">
                 </a>
             </div>
 
-            <div class="col-xs-3 col-md-4 text-center">
+            <div class="col-4 col-md-4 text-center">
                 <i class="fas fa-map-marker-alt"></i>
                 <span>Milano</span>
             </div>
 
             @if (Route::has('login'))
-                <div class="col-xs-5 col-md-4 text-right">
+                <div class="col-4 col-md-4 text-right">
                     @auth
                         <a href="{{ url('/admin') }}">Home</a>
                     @else
