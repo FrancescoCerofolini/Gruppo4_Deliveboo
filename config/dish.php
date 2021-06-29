213f6605448d424ebcf6c7e0c742de41e6f357cd
laura-caldaronello <caldaronello.l@gmail.com>
2021-06-29 11:15:45 +0200

    ok

--- config/dish.php
+++ config/dish.php
@@ -1,138 +1,79 @@
 <?php
 return
-    // $name = 
     [
-    [
-        "angelo",
-        "margherita",
-        "capricciosa",
-        "rucola e scaglie",
-        "4 formaggi",
-        // "deliziosa",
-        // "biancaneve",
-        // "pistacchio",
-        // "golden",
-        // "diavola",
-        // "etnea",
-        // "president",
-        // "speck e mascarpone",
-        // "4 formaggi doc",
-        //pizzeria
-        "chilaquiles",
-        "pancho villa",
-        "ciauhtemoc",
-        "zapata",
-        "revoluction",
-        // "frida",
-        // "don venustiano",
-        // "le maliche",
-        // "fajita",
-        // "chimichanga",
-        //messicano
-        "sashimi salmone",
-        "uramaki tonno",
-        "hosomaki surimi",
-        "shifudo udon",
-        "nigiri gambero",
-        // "gunkan philadephia",
-        //cinese
-        "antipasto di mare",
-        "cocktail di gamberetti",
-        "pepata di cozze e vongole",
-        "spaghetti allo scoglio",
-        "linguine alle sarde",
-        // "Strigoli gamberetti e zafferano",
-        // "Grigliata mista",
-        // "Spiedini misti gamberetti e calamari",
-        // "Baccalà fritto",
-        // "Gamberoni",
-        //pesce
-        "zakiri",
-        "hammus",
-        "greek salad",
-        "chicken shish salad",
-        "falafed salad",
-        // "gyro salad",
-        // "falafed pita",
-        // "mediterranean pita",
-        // "iskended kebab",
-        // "kofte plate",
-        //kebab
-        "chicago",
-        "illinois",
-        "missouri",
-        "kansas",
-        "oklahoma",
-        // "texas",
-        // "arizona",
-        // "new mexico",
-        // "california",
-        // "colorado",
-        //hamburgeria
-        "deliziosa",
-        "biancaneve",
-        "pistacchio",
-        "golden",
-        "diavola",
-        //pizzeria
-        "tagliata di filetto",
-        "involtino di vitello",
-        "galletto hamburghese",
-        "gulash",
-        "costolette di agnello",
-        // "taglere di salumi e formaggi",
-        // "pollo disossato",
-        // "fiorentina",
-        // "angus",
-        // "involtino al pistacchio",
-        //carne
-        "tortello",
-        "maltagliati",
-        "polpo alla brace",
-        "filetto di ombrina",
-        "cacio e pepe",
-        // "ostriche rosa",
-        // "costolette di manzo e maiale",
-        //luxury
-        "nigiri salmone",
-        "hosomaki tonno",
-        "chirashi branzino",
-        "temaki salmone",
-        "zuppa di misu",
-        //sushi
-        
-        // "leggenda",
-        // "belpaese",
-        // "provenzale",
-        // "l'estate",
-        // "il mare",
-        // "saporita",
-        // "raffinata",
-        // "verdissima",
-        // "campagnola",
-        // "primavera",
-        // //piadine
-        // "coppa cioccolato",
-        // "coppa bacio perugina",
-        // "sorbetto ala limone",
-        // "souffle cioccolato",
-        // "crema catalana",
-        // "coppa nocciola",
-        // "parfe di mandorle",
-        // "parfe di pistacchio",
-        // "sorbetto al cocco",
-        // "sacher",
-        // //dolci
-        
-        
+        [
+            //pizzeria
+            "angelo",
+            "margherita",
+            "capricciosa",
+            "rucola e scaglie",
+            "4 formaggi",
+            
+            //messicano
+            "chilaquiles",
+            "pancho villa",
+            "ciauhtemoc",
+            "zapata",
+            "revoluction",
 
+            //cinese
+            "sashimi salmone",
+            "uramaki tonno",
+            "hosomaki surimi",
+            "shifudo udon",
+            "nigiri gambero",
 
+            //pesce
+            "antipasto di mare",
+            "cocktail di gamberetti",
+            "pepata di cozze e vongole",
+            "spaghetti allo scoglio",
+            "linguine alle sarde",
 
-    ],
-    // $allergens = 
-    ['cereali e derivati','uova','arachidi','soia','latte','frutta a guscio','sedano','senape','sesamo']];
-    // $dishes = [
-    //     "name" => $name,
-    //     "allergens" => $allergens
-    // ];
+            //kebab
+            "zakiri",
+            "hammus",
+            "greek salad",
+            "chicken shish salad",
+            "falafed salad",
+
+            //hamburgeria
+            "chicago",
+            "illinois",
+            "missouri",
+            "kansas",
+            "oklahoma",
+
+            //pizzeria
+            "deliziosa",
+            "biancaneve",
+            "pistacchio",
+            "golden",
+            "diavola",
+
+            //carne
+            "tagliata di filetto",
+            "involtino di vitello",
+            "galletto hamburghese",
+            "gulash",
+            "costolette di agnello",
+
+            //luxury
+            "tortello",
+            "maltagliati",
+            "polpo alla brace",
+            "filetto di ombrina",
+            "cacio e pepe",
+
+            //sushi
+            "nigiri salmone",
+            "hosomaki tonno",
+            "chirashi branzino",
+            "temaki salmone",
+            "zuppa di misu",
+        ],
+        
+        // allergeni 
+        ['cereali e derivati','uova','arachidi','soia','latte','frutta a guscio','sedano','senape','sesamo']
+    ];
 ?>
